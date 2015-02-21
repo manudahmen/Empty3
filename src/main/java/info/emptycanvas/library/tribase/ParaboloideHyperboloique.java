4a6c18603f24751f055b5f216541b45fa945c41b
mdahmen <manuel.dahmen@gmail.com>
2015-02-21 14:47:33 +0100

    Nouvel objet
    Nouvelle classe pour le paraboloïde hyperbolique (c'est maman qui me l'a
    expliqué ce matin :-)

---
+++ src/main/java/info/emptycanvas/library/tribase/ParaboloideHyperboloique.java
@@ -0,0 +1,43 @@
+/***
+Global license : 
+
+    Microsoft Public Licence
+    
+    author Manuel Dahmen <manuel.dahmen@gmail.com>
+
+***/
+
+
+package info.emptycanvas.library.tribase;
+
+import info.emptycanvas.library.nurbs.ParametrizedSurface;
+import info.emptycanvas.library.object.Point3D;
+
+/**
+ *
+ * @author Manuel Dahmen <manuel.dahmen@gmail.com>
+ */
+public class ParaboloideHyperboloique extends ParametrizedSurface {
+    private double a;
+    private double b;
+    private double h;
+
+    public ParaboloideHyperboloique(double a, double b, double h) {
+        this.a = a;
+        this.b = b;
+        this.h = h;
+    }
+
+    @Override
+    public Point3D calculerPoint3D(double u, double v) {
+        return new Point3D(a/2+(u+v), b/2*(u-v), h*u*v);
+    }
+
+    @Override
+    public Point3D calculerVitesse3D(double u, double v) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+
+}
