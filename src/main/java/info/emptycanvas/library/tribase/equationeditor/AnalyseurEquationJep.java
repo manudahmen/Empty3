fbc33aa5cb5ba35eac5c4d7b886ee8da5fda9a09
mdahmen <manuel.dahmen@gmail.com>
2015-02-25 22:45:43 +0100

    JEP parser skeleton class

---
+++ src/main/java/info/emptycanvas/library/tribase/equationeditor/AnalyseurEquationJep.java
@@ -0,0 +1,65 @@
+/***
+Global license : 
+
+    Microsoft Public Licence
+    
+    author Manuel Dahmen <manuel.dahmen@gmail.com>
+
+***/
+
+
+package info.emptycanvas.library.tribase.equationeditor;
+
+import org.nfunk.jep.JEP;
+
+/**
+ *
+ * @author Manuel Dahmen <manuel.dahmen@gmail.com>
+ */
+public class AnalyseurEquationJep {
+    private final JEP myParser;
+    private String strParser;
+
+    public AnalyseurEquationJep() {
+        myParser = new org.nfunk.jep.JEP();
+       myParser.addStandardFunctions();
+       myParser.addStandardConstants();
+    }
+   public void parse(String parse)
+   {
+       strParser = parse;
+   }
+   public double value()
+   {
+       myParser.parseExpression(strParser);
+       return myParser.getValue();
+   }
+   public void setVariable(String name, double v)
+   {
+       myParser.addVariable(name, v);
+   }
+   public void setContant(String name, double v)
+   {
+       myParser.addVariable(name, v);
+   }
+    public static void main(String[] args) {
+        
+    AnalyseurEquationJep anlayseurEquationJep = new AnalyseurEquationJep();
+    anlayseurEquationJep.parse("a*x+b*b");
+
+    anlayseurEquationJep.setContant("a", 1);
+    anlayseurEquationJep.setContant("b", 2);
+    
+    
+    anlayseurEquationJep.setContant("x", 3);
+    System.out.println("Result: " + anlayseurEquationJep.value()+ "( expected: 7");
+    anlayseurEquationJep.setContant("x", 4);
+    System.out.println("Result: " + anlayseurEquationJep.value() + "(expected= 8");
+    anlayseurEquationJep.setContant("x", 5);
+    System.out.println("Result: " + anlayseurEquationJep.value() + "(expected= 9");
+    
+    
+    }
+    
+    
+}
